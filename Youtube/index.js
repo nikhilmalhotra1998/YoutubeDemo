6a7c333559087e8cedea4346812b8924053a1e9b
Nikhil Malhotra <nikhilmalhotra.0111@gmail.com>
2020-02-25 13:19:45 +0530

    Delete index.js

--- Youtube/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import  videosReducer  from './reducers/dataReducers.js';
-
-const rootReducer = combineReducers({
- result: videosReducer
-})
-
-export default rootReducer;
